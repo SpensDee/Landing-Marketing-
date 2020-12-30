7bb3bb4eda232de7a0e5262054c34dc80d8bbd54
SpensDee <71296101+SpensDee@users.noreply.github.com>
2020-12-30 22:59:51 +0200

    test
    test

---
+++ marketing/js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    const mixer = mixitup('.portfolio__content', {
+            animation: {
+                duration: 938,
+                nudge: true,
+                reverseOut: false,
+                effects: "fade translateZ(-15px) rotateX(90deg) rotateY(90deg)"
+            }
+    });
+});
