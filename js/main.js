bb76a64d9f098eea155ec8121ff90758da2728cf
SpensDee <71296101+SpensDee@users.noreply.github.com>
2021-01-02 23:21:57 +0200

    new

--- js/main.js
+++ js/main.js
@@ -7,4 +7,10 @@ $(function(){
                 effects: "fade translateZ(-15px) rotateX(-90deg) rotateY(-90deg)"
             }
     });
+
+    $('.slider-blog__inner').slick({
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/prevArrow.svg" alt="arrow_left"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/nextArrow.svg" alt="arrow_right"></button>'
+    });
 });
