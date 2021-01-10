3db786482021035e27b31bc08773ecaf93ecd983
SpensDee <71296101+SpensDee@users.noreply.github.com>
2021-01-10 13:48:06 +0200

    adaptive

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
 $(function(){
-    const mixer = mixitup('.portfolio__content', {
-            animation: {
-                duration: 938,
-                nudge: true,
-                reverseOut: false,
-                effects: "fade translateZ(-15px) rotateX(-90deg) rotateY(-90deg)"
-            }
-    });
 
     $('.slider-blog__inner').slick({
         dots: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="images/prevArrow.svg" alt="arrow_left"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="images/nextArrow.svg" alt="arrow_right"></button>'
     });
+
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').toggleClass('menu__list--active')
+    });
+
+    const mixer = mixitup('.portfolio__content', {
+        animation: {
+            duration: 938,
+            nudge: true,
+            reverseOut: false,
+            effects: "fade translateZ(-15px) rotateX(-90deg) rotateY(-90deg)"
+        }
+    });
+
 });
