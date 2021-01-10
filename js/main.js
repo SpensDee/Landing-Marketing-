28fd0bdc5ad9947547577a7a7488d57515e8c367
SpensDee <71296101+SpensDee@users.noreply.github.com>
2021-01-10 14:57:28 +0200

    btn

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(function(){
     });
 
     $('.menu__btn').on('click', function(){
-        $('.menu__list').toggleClass('menu__list--active')
+        $('.header__top-inner').toggleClass('header__top-inner--active');
     });
 
     const mixer = mixitup('.portfolio__content', {
